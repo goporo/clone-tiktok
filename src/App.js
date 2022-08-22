2eefb27de551afd0eb66fff7c29ca1cfc12ae37b
goporo <goporolo1@gmail.com>
2022-08-22 19:47:17 +0700

    test commit with git desktop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+
+
+  const [job, setJob] = useState('')
+  const [jobs, setJobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem('job'))
+    return storageJobs ?? ['xin viec fb']
+  })
+  const handleAddJob = () => {
+
+    setJobs(prev => {
+      const newJobs = [...prev, job]
+      localStorage.setItem('job', JSON.stringify(newJobs))
+      return newJobs
+    })
+    setJob('')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>helo i'm wjbu jack and oggy</h1>
+      <img src="https://c.tenor.com/dJEIQACw138AAAAC/loli-ogusers.gif" alt="anh loli" />
+      <br />
+      <input style={{ width: 500, backgroundColor: "coral", color: '#333' }}
+        placeholder={'Nhap 1 job'}
+        value={job}
+        onChange={(e) => setJob(e.target.value)}
+      />
+      <button onClick={handleAddJob}>ADD</button>
+      <ul>
+        {jobs.map((job, index) => (
+          <li key={index}>{job}</li>
+        ))}
+      </ul>
     </div>
+
   );
 }
 
